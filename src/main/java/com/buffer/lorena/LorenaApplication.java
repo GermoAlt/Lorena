82c37ea98d5a4a83a468445a75017f577ba5395d
German <germanaltairac@hotmail.com>
2021-04-03 20:02:31 -0300

    spring initializr files

---
+++ src/main/java/com/buffer/lorena/LorenaApplication.java
@@ -0,0 +1,13 @@
+package com.buffer.lorena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorenaApplication.class, args);
+	}
+
+}
