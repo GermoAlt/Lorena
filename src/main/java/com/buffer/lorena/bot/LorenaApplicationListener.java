16af9f36c6e75ee9433b7563f114d75a2a05cbc0
German <germanaltairac@hotmail.com>
2021-04-05 00:02:14 -0300

    remove dotenv, using application.properties file

--- src/main/java/com/buffer/lorena/bot/LorenaApplicationListener.java
+++ src/main/java/com/buffer/lorena/bot/LorenaApplicationListener.java
@@ -2,6 +2,8 @@ package com.buffer.lorena.bot;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -9,9 +11,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LorenaApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
+
     Logger logger = LogManager.getLogger(LorenaApplicationListener.class);
+    @Autowired
+    MessageHandler messageHandler;
+    @Value("${lorena.token}")
+    String token;
+
+    public LorenaApplicationListener(MessageHandler messageHandler) {
+        this.messageHandler = messageHandler;
+    }
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         logger.info("ITS ALIVE!");
+        messageHandler.init(token);
     }
 }
